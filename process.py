383541cf5de97881a82e91506ef540183844fe24
GastonZalba <gastonzalba@outlook.com>
2024-04-11 13:46:57 -0300

    added disablemask argument

--- process.py
+++ process.py
@@ -14,6 +14,7 @@ input_folder = 'input'
 output_folder = 'output'
 disable_overviews = False
 disable_tfw = False
+disable_mask = False
 format = 'grayscale'
 extensions = ['.tif', '.tiff', '.vrt', '.img']
 
@@ -30,6 +31,8 @@ parser.add_argument('--disableoverviews', action='store_true', default=disable_o
                     help='Add overviews to the export (default: %(default)s)')
 parser.add_argument('--disabletfw', action='store_true', default=disable_tfw,
                     help='Export TFW file (default: %(default)s)')
+parser.add_argument('--disablemask', action='store_true', default=disable_mask,
+                    help='Export file with internal mask (default: %(default)s)')
 
 args = parser.parse_args()
 input_folder = args.input
@@ -37,6 +40,7 @@ output_folder = args.output
 format = args.format
 disable_overviews = args.disableoverviews
 disable_tfw = args.disabletfw
+disable_mask = args.disablemask
 
 if not os.path.exists(input_folder):
     sys.exit(f'ERROR: folder {input_folder} has not be found')
@@ -102,8 +106,6 @@ for subdir, dirs, files in os.walk(input_folder):
                         'tfw': 'YES' if not disable_tfw else 'NO'
                     })
 
-                    internal_mask = np.asarray(np.where(dem == noDataValue, False, True))
-
                     if format == 'mapbox':
                         r += np.floor_divide((100000 + dem * 10), 65536)
                         g += np.floor_divide((100000 + dem * 10), 256) - r * 256
@@ -114,12 +116,18 @@ for subdir, dirs, files in os.walk(input_folder):
                         r += np.floor_divide(dem, 256)
                         g += np.mod(dem, 256)
                         b += np.floor((dem - np.floor(dem)) * 256)
-
+                        
+                    else:
+                        sys.exit(f'"{format}" is no valid format')
+                    
                     with rasterio.open(out, 'w', **meta) as dst:
                         dst.write_band(1, r.astype(rasterio.uint8))
                         dst.write_band(2, g.astype(rasterio.uint8))
                         dst.write_band(3, b.astype(rasterio.uint8))
-                        dst.write_mask(internal_mask)
+                        
+                        if not disable_mask:
+                            internal_mask = np.asarray(np.where(dem == noDataValue, False, True))
+                            dst.write_mask(internal_mask)
 
                         if not disable_overviews:
                             dst.build_overviews(
